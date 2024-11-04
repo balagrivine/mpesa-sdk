06a8ec2c224144b44a9c63d3da3ad49f6a31df76
balagrivine <balagrivine@gmail.com>
2024-11-04 01:36:22 -0800

    chore(refactor): change package structure for easy API access

---
+++ mpesa/__init__.py
@@ -0,0 +1,4 @@
+from .api import MpesaBase, C2B, B2C, TransactionStatus, Balance
+
+__all__ = ["MpesaBase", "C2B", "B2C", "TransactionStatus", "Balance"]
+
---
+++ mpesa/api/__init__.py
@@ -0,0 +1,7 @@
+from .auth import MpesaBase
+from .balance import Balance
+from .status import TransactionStatus
+from .c2b import C2B
+from .b2c import B2C
+
+__all__ = ["MpesaBase", "Balance", "TransactionStatus", "C2B", "B2C"]
